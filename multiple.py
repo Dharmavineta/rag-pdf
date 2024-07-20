fa96c0a1bc66fb01f850b6a19adfe73ca5079b33
Krishna Bharadwaj <krishnabharadwaj1995@gmail.com>
2024-07-20 21:19:40 +0530

    pdf-logic

--- multiple.py
+++ multiple.py
@@ -7,6 +7,27 @@ from langchain_community.embeddings import OllamaEmbeddings
 from langchain.prompts import ChatPromptTemplate
 from langchain.schema.runnable import RunnablePassthrough
 from langchain.schema.output_parser import StrOutputParser
+from PyPDF2 import PdfReader
+from langchain_text_splitters import TextSplitter
+
+def get_pdf_text(pdf_docs):
+    print("hello")
+    text=""
+    for pdf in pdf_docs:
+        pdf_reader= PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text+=page.extract_text()
+        
+
+    return text
+        
+
+
+def get_text_chunks(raw_text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=5000, chunk_overlap=100, separators="\n")
+    chunks=text_splitter.split_text(raw_text)
+    return chunks
+
 
 
 def main():
@@ -16,8 +37,15 @@ def main():
     
     with st.sidebar:
         st.subheader("Your documents")
-        st.file_uploader("Upload your PDFs here and click on process")
-        st.button("Process")
+        pdf_docs= st.file_uploader("Upload your PDFs here and click on process", accept_multiple_files=True)
+        if st.button("Process"):
+            with st.spinner("Processing"):
+                raw_text= get_pdf_text(pdf_docs)
+
+
+                text_chunks= get_text_chunks(raw_text)
+                
+            
 
 if __name__ == '__main__':
     main()
