df5bd9df7d7b0436b0b6747b4a138b2d55de68a8
Krishna Bharadwaj <krishnabharadwaj1995@gmail.com>
2024-07-20 20:47:25 +0530

    langchain

--- multiple.py
+++ multiple.py
@@ -1,17 +1,18 @@
 import streamlit as st
+from langchain_community.chat_models import ChatOllama
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_community.vectorstores import FAISS
+
+
 def main():
-    st.set_page_config(page_title="Chat with multiple PDF's", page_icon=":books:")
-    st.header("Chat with multiple PDF's :books:")
+    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
+    st.header("Chat with multiple PDFs :books:")
     st.text_input("Ask a question about your document")
     
-
     with st.sidebar:
         st.subheader("Your documents")
-        st.file_uploader("Upload your PDF's here and click on process")
-
-
-    
-
+        st.file_uploader("Upload your PDFs here and click on process")
+        st.button("Process")
 
 if __name__ == '__main__':
     main()
