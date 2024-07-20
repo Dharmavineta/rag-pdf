27d4d0ce916dd0f36be8a04112eafc3bf6fc4816
Krishna Bharadwaj <krishnabharadwaj1995@gmail.com>
2024-07-20 20:49:06 +0530

    dep added

--- multiple.py
+++ multiple.py
@@ -2,6 +2,11 @@ import streamlit as st
 from langchain_community.chat_models import ChatOllama
 from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema.runnable import RunnablePassthrough
+from langchain.schema.output_parser import StrOutputParser
 
 
 def main():
